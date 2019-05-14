c7526d4b1c9540cf30f64b940d969d19cd16004b
Matthew Lin <mlin367@yahoo.com>
2019-05-13 21:24:14 -0700

    added redis for session storage

--- server/index.js
+++ server/index.js
@@ -4,8 +4,23 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./graphql/schema');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const redis = require('redis');
+const redisClient = redis.createClient();
 
 const app = express();
+
+redisClient.on('error', err => console.error('Redis error: ', err));
+
+app.use(session({
+  secret: 'shhhhhcatz',
+  name: 'social_media_clone',
+  resave: false,
+  store: new RedisStore({
+    client: redisClient
+  })
+}))
 
 app.use('/graphql', graphqlHTTP({
   schema,
