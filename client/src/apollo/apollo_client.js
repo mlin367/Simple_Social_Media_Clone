bfa15e1d920a5a34e1fe7c24379d30d381dcf312
Matthew Lin <mlin367@yahoo.com>
2019-05-23 00:54:50 -0700

    cache is reset everytime for a diff page, need to fix

--- client/src/apollo/apollo_client.js
+++ client/src/apollo/apollo_client.js
@@ -1,7 +1,13 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:2400/graphql'
+  uri: 'http://localhost:2400/graphql',
+  clientState: {
+    defaults: {
+      currThreadId: 1
+    },
+    resolvers: {}
+  }
 });
 
 export default client;
--- client/src/pages/thread/components/ThreadApp.jsx
+++ client/src/pages/thread/components/ThreadApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from '../../../common_components/NavBar';
 import { Query, Mutation } from 'react-apollo';
-import { GET_AUTH_STATUS } from '../../../apollo/queries';
+import { GET_AUTH_STATUS, GET_THREAD_COMMENTS } from '../../../apollo/queries';
 import { gql } from "apollo-boost";
 
 const GET_THREAD_ID = gql`
@@ -21,11 +21,21 @@ const ThreadApp = props => (
         return data.isLoggedIn.status ? <textarea placeholder='comment here'/> : <h3>You need to be logged in to comment</h3>;
       }}
     </Query>
-    {/* <Query query={GET_THREAD_ID}>
-      {({ data : { currThreadId }}) => (
-
-      )}
-    </Query> */}
+    <Query query={GET_THREAD_ID}>
+      {({ loading: loading1, data: { currThreadId} }) => {
+        if (loading1) return <h1>Loading...</h1>
+        return (
+        <Query query={GET_THREAD_COMMENTS} variables={{ id: currThreadId }}>
+          {({ loading: loading2, data: { thread } }) => {
+            if (loading2) return <h1>Loading...</h1>
+            return thread.comments.map(comment => (
+              <p>{comment.text}</p>
+            ))
+          }}
+        </Query>
+        )
+      }}
+    </Query>
   </div>
 );
 
