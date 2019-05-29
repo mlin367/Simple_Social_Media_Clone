bc99a88e2584d63b351832d4084c401bffb2828a
Matthew Lin <mlin367@yahoo.com>
2019-05-28 19:10:44 -0700

    Refactored threadapp to include the jsxk in the return function

--- client/src/pages/thread/components/ThreadApp.jsx
+++ client/src/pages/thread/components/ThreadApp.jsx
@@ -5,37 +5,6 @@ import { GET_AUTH_STATUS, GET_THREAD_COMMENTS } from '../../../apollo/queries';
 import { ADD_COMMENT } from '../../../apollo/mutations';
 import Comment from '../../../common_components/Comment';
 
-const submitComment = (text, userId) => (
-  <Mutation
-    mutation={ADD_COMMENT}
-    variables={{ text, userId, threadId: localStorage.getItem('currThreadId') }}
-    refetchQueries={() => [
-      {
-        query: GET_THREAD_COMMENTS,
-        variables: { id: localStorage.getItem('currThreadId') }
-      }
-    ]}
-  >
-    {addComment => <button onClick={() => addComment()}>Submit</button>}
-  </Mutation>
-);
-
-const commentPermission = (onChangeFunc, text) => (
-  <Query query={GET_AUTH_STATUS}>
-    {({ loading, data }) => {
-      if (loading) return <h1>Loading</h1>;
-      return data.isLoggedIn.status ? (
-        <div className="commentText">
-          <textarea onChange={onChangeFunc} placeholder="comment here" />
-          {submitComment(text, data.isLoggedIn.userId)}
-        </div>
-      ) : (
-        <h3>You need to be logged in to comment</h3>
-      );
-    }}
-  </Query>
-);
-
 class ThreadApp extends React.Component {
   constructor(props) {
     super(props);
@@ -52,6 +21,43 @@ class ThreadApp extends React.Component {
   }
 
   render() {
+
+    const submitComment = userId => (
+      <Mutation
+        mutation={ADD_COMMENT}
+        variables={{ text: this.state.text, userId, threadId: localStorage.getItem('currThreadId') }}
+        refetchQueries={() => [
+          {
+            query: GET_THREAD_COMMENTS,
+            variables: { id: localStorage.getItem('currThreadId') }
+          }
+        ]}
+      >
+        {addComment => <button onClick={() => {
+          addComment();
+          this.setState({
+            text: ''
+          });
+        }}>Submit</button>}
+      </Mutation>
+    );
+
+    const commentPermission = (
+      <Query query={GET_AUTH_STATUS}>
+        {({ loading, data }) => {
+          if (loading) return <h1>Loading</h1>;
+          return data.isLoggedIn.status ? (
+            <div className="commentText">
+              <textarea value={this.state.text} onChange={this.onInputChange} placeholder="comment here" />
+              {submitComment(data.isLoggedIn.userId)}
+            </div>
+          ) : (
+            <h3>You need to be logged in to comment</h3>
+          );
+        }}
+      </Query>
+    );
+    
     return (
       <div className="threadApp">
         <NavBar />
@@ -65,7 +71,7 @@ class ThreadApp extends React.Component {
               <React.Fragment>
                 <h2>{thread.title}</h2>
                 <p>{thread.description}</p>
-                {commentPermission(this.onInputChange, this.state.text)}
+                {commentPermission}
                 <h3>Comments</h3>
                 {thread.comments.map(comment => (
                   <Comment comment={comment} key={Math.random()} />
